0f81246fe42841a7c02c4a63803b035ccc79c508
Mike Shafter <yuhang.061@gmail.com>
2022-08-23 15:18:51 +0900

    Start api

---
+++ src/main/java/mikeshafter/iciwi/Version/VersionWrapper.java
@@ -0,0 +1,9 @@
+package mikeshafter.mikeshafter.iciwi.mikeshafter.mikeshafter.iciwi.Version;
+
+import org.bukkit.entity.Player;
+
+public interface VersionWrapper {
+  int getContainerId(Player player,Object container);
+
+  
+}
