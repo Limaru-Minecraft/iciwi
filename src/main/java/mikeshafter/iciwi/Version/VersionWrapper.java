7db9f7b3f5ed8a7598b73dfbdfc1c4294ba3b564
Mike Shafter <yuhang.061@gmail.com>
2022-08-23 08:14:53 +0000

    Yank TC-Coasters code to handle anvil GUIs

--- src/main/java/mikeshafter/iciwi/Version/VersionWrapper.java
+++
@@ -1,9 +0,0 @@
-package mikeshafter.mikeshafter.iciwi.mikeshafter.mikeshafter.iciwi.Version;
-
-import org.bukkit.entity.Player;
-
-public interface VersionWrapper {
-  int getContainerId(Player player,Object container);
-
-  
-}
