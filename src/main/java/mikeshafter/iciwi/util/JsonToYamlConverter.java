8df65fd4cfdb1e79b9e7ae1482e01b879eddc48d
Mike Shafter <yuhang.061@gmail.com>
2022-09-02 03:14:36 +0000

    write json to yaml converter

--- src/main/java/mikeshafter/iciwi/util/JsonToYamlConverter.java
+++ src/main/java/mikeshafter/iciwi/util/JsonToYamlConverter.java
@@ -1,7 +1,33 @@
 package mikeshafter.iciwi.util;
 
+import mikeshafter.iciwi.Iciwi;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class JsonToYamlConverter {
   public static void main() {
-  
+    File jsonFile = new File(Iciwi.getPlugin(Iciwi.class).getDataFolder(), "fares.json");
+    File yamlFile = new File(Iciwi.getPlugin(Iciwi.class).getDataFolder(), "fares.yml");
+
+    try {
+      Scanner scanner = new Scanner(jsonFile);
+      StringBuilder jsonBuilder = new StringBuilder();
+      while (scanner.hasNextLine()) jsonBuilder.append(scanner.nextLine());
+      scanner.close();
+      String json = jsonBuilder.toString();
+
+      // convert all doubles to {"Second Class": double}
+      json = json.replaceAll("(\\d+\\.\\d+)", "{'Second Class': $1 }");
+
+      // write to file
+      FileWriter fileWriter = new FileWriter(yamlFile);
+      fileWriter.write(json);
+      fileWriter.close();
+    
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 }
