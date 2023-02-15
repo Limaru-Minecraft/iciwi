e95b2d700dda7ca8cfca80bcfed279469d667f74
Mike Shafter <yuhang.061@gmail.com>
2023-02-15 22:27:07 +0900

    Fix deprecation in the faregates package (#40)
    * remove deprecations
    
    * do what the IDE tells me to do

--- src/main/java/mikeshafter/iciwi/util/MachineUtil.java
+++ src/main/java/mikeshafter/iciwi/util/MachineUtil.java
@@ -6,19 +6,27 @@ import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 import java.util.regex.Pattern;
 
 
 public class MachineUtil {
   public static boolean isDouble(String s) {
-    return Pattern.matches(("[\\x00-\\x20]*"+"[+-]?("+"NaN|"+"Infinity|"+"((((\\p{Digit}+)(\\.)?((\\p{Digit}+)?)([eE][+-]?(\\p{Digit}+))?)|"+"(\\.(\\p{Digit}+)([eE][+-]?(\\p{Digit}+))?)|"+"(("+"(0[xX](\\p{XDigit}+)(\\.)?)|"+"(0[xX](\\p{XDigit}+)?(\\.)(\\p{XDigit}+))"+")[pP][+-]?(\\p{Digit}+)))"+"[fFdD]?))"+"[\\x00-\\x20]*"), s);
+    return Pattern.matches(("[\\x00-\\x20]*"+"[+-]?("+"NaN|"+"Infinity|"+"((((\\d+)(\\.)?((\\d+)?)([eE][+-]?(\\d+))?)|"+"(\\.(\\d+)([eE][+-]?(\\d+))?)|"+"(("+"(0[xX](\\p{XDigit}+)(\\.)?)|"+"(0[xX](\\p{XDigit}+)?(\\.)(\\p{XDigit}+))"+")[pP][+-]?(\\d+)))"+"[fFdD]?))"+"[\\x00-\\x20]*"), s);
   }
   
   public static String parseComponent(Component c) {
     if (c instanceof TextComponent) return ((TextComponent) c).content();
     else return c.examinableName();
+  }
+
+  public static List<String> parseComponents(List<Component> componentList) {
+    List<String> r = new ArrayList<>();
+    componentList.forEach(c -> r.add(parseComponent(c)));
+    return r;
   }
   
   public static <E> boolean any(Collection<E> c, Collection<E> k) {
