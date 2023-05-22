c8e79f22605190b385c733e89de990e0dcbe95e4
Mineshafter61 <yuhang.061@gmail.com>
2023-05-22 08:36:45 +0000

    staph

--- src/main/java/mikeshafter/iciwi/tickets/CustomMachine.java
+++ src/main/java/mikeshafter/iciwi/tickets/CustomMachine.java
@@ -112,8 +112,11 @@ public class CustomMachine implements Machine {
       var entry = fareIterator.next();
       var item = makeItem(Material.PAPER, 0, Component.text(entry.getKey()), Component.text(entry.getValue()));
       this.clickables[i] = Clickable.of(item, (event) -> {
+        // generate ticket
         ItemStack ticket = generateTicket(station, end, parseComponent(item.getItemMeta().displayName()));
-        if (ticket != null) player.getInventory().addItem(generateTicket(station, end, parseComponent(item.getItemMeta().displayName()) ));
+        // add item to player's inventory
+        if (ticket != null) player.getInventory().addItem(ticket);
+        // the drill on last step
         event.setCancelled(true);
         player.closeInventory();
       });
@@ -132,13 +135,13 @@ public class CustomMachine implements Machine {
 
       // find owners of the current station and deposit accordingly
       List<String> ownersList = owners.getOwners(from);
-      for (String owner : ownersList)
-        owners.deposit(owner, price / 2 / ownersList.size());
+      for (String owner : ownersList.size())
+        owners.deposit(owner, (price >> 1) / ownersList.size());
 
       // find owners of the station the ticket goes to and deposit accordingly
       ownersList = owners.getOwners(to);
-      for (String owner : ownersList)
-        owners.deposit(owner, price / 2 / ownersList.size());
+      for (String owner : ownersList.size())
+        owners.deposit(owner, (price >> 1) / ownersList.size());
       
       // Get ticket materials
       Material ticketMaterial = Material.valueOf(plugin.getConfig().getString("ticket.material"));
@@ -154,5 +157,117 @@ public class CustomMachine implements Machine {
       return null;
     }
   }
+  
+  private class EventListener implements Listener {
+    
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onInventoryClick(InventoryClickEvent event) {
+      Inventory inventory = event.getClickedInventory();
+      ItemStack item = event.getCurrentItem();
+      if (inventory == null) return;
+      
+      if (item != null && item.hasItemMeta() && item.getItemMeta() != null) {
+        inventory.close();
+        setTerminal(item.getItemMeta().displayName());
+        CommonUtil.unregisterListener(this);
+        selectClass();
+      }
+    }
+  }
+
+  /**
+   * Sort an Iterable based on each string's relevance.
+   */
+  public String[] relevanceSort(String pattern, String[] values) {
+    Arrays.sort(values, (v1, v2) -> Float.compare(relevance(pattern, v2), relevance(pattern, v1)));
+    return values;
+  }
+
+  /**
+   * Puts the items of a clickable[] into an inventory.
+   *
+   * @param clickables The clickable[] stated above.
+   * @param inventory  The inventory stated above.
+   */
+  private void setItems(Clickable[] clickables, Inventory inventory) {
+    ItemStack[] items = new ItemStack[clickables.length];
+    for (int i = 0; i < clickables.length; i++)
+      if (clickables[i] != null) items[i] = clickables[i].getItem();
+    inventory.setStorageContents(items);
+  }
+
+  /**
+   * Relevance function
+   */
+  public float relevance(String pattern, String term) {
+    // Ignore case
+    pattern = pattern.toLowerCase();
+    term = term.toLowerCase();
+    
+    // Optimisation
+    if (term.equals(pattern)) return 1f;
+
+    /*
+    Search = the pattern term
+    Match = a string containing the pattern term
+    term.length() >= pattern.length()
+    Relevance = percentage of letters equal to the sequence in searchResult.
+    */
+    
+    // Required variables
+    int searchLength = pattern.length();
+    int matchLength = term.length();
+    
+    // If the term contains the pattern term, it is relevant, thus we give a full score
+    if (term.contains(pattern)) return ((float) searchLength)/matchLength;
+    
+    // If the term does not contain the pattern term, but contains parts of it, we give a divided score
+    // The score is calculated by s_x/x*m where s is the pattern term length, x is the number of characters in the pattern term not matched,
+    //   and m is the term length.
+    
+    /* At this point term does not contain pattern */
+    for (int i = searchLength; i >= 2; i--) { // i is length of substring
+      for (int j = 0; j+i <= searchLength; j++) {
+        String subSearch = pattern.substring(j, j+i);
+        if (term.contains(subSearch)) {
+          // found term, calculate relevance
+          return ((float) i)/(searchLength-i)/matchLength;
+        }
+      }
+    }
+    
+    // if no term found, return 0f (pattern failed)
+    return 0f;
+  }
+  
+  public void setTerminal(Component terminal) {
+    this.terminal = terminal;
+  }
+
+  @Override
+  public Clickable[] getClickables() {
+    return clickables;
+  }
+
+  @Override
+  public boolean useBottomInv() {
+    return false;
+  }
+
+  @Override
+  public void setSelectedItem(ItemStack selectedItem) {
+  }
+
+  @Override
+  public ItemStack getSelectedItem() {
+    return null;
+  }
+
+  @Override
+  public void onCardSelection() {
+  }
+
+  @Override
+  public void setBottomInv(boolean b) {}
   
 }
