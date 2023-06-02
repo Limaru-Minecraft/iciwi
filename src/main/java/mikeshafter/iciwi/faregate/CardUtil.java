1bb62a4036126494ca4b94bb77633e7c7d134a47
Mineshafter61 <yuhang.061@gmail.com>
2023-06-02 17:04:34 +0900

    write member and half of transfer signs

--- src/main/java/mikeshafter/iciwi/faregate/CardUtil.java
+++ src/main/java/mikeshafter/iciwi/faregate/CardUtil.java
@@ -5,7 +5,6 @@ import mikeshafter.iciwi.CardSql;
 import mikeshafter.iciwi.Iciwi;
 import mikeshafter.iciwi.api.IcCard;
 
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
